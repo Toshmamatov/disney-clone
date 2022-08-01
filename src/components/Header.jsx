79cd3389fe59effbe1100b5e9b9c993d4f0b4a6b
Toshmamatov <toshmamatov03@gmail.com>
2022-08-01 05:57:31 -0700

    adding header-com

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return <Container>Hello Header</Container>;
+};
+
+export default Header;
+
+const Container = styled.div`
+  height: 70px;
+  background-color: #090b13;
+`;
