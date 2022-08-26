c27d18a538fd37af2ad60d4ebc6196fa92ff1755
Toshmamatov <toshmamatov03@gmail.com>
2022-08-26 02:49:48 -0700

    last changes

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import { selectMovies } from "../features/movie/movieSlice";
+import { useSelector } from "react-redux/es/exports";
+import { Link } from "react-router-dom";
 
 const Movies = () => {
+  const movies = useSelector(selectMovies);
+  console.log("movies", movies);
   return (
     <Container>
       <h4>Recomended for you</h4>
       <Content>
+        {movies &&
+          movies.map((movie) => (
+            <Wrap>
+              <Link to={`/detail/${movie.id}`}>
+                <img src={movie.cardImg} alt="recommended-movies" />
+              </Link>
+            </Wrap>
+          ))}
         <Wrap>
-          <img
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
-            alt="recommended-movies"
-          />
+          <Link to="/detail">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
+              alt="recommended-movies"
+            />
+          </Link>
         </Wrap>
         <Wrap>
           <img
